1e850dac6ab2992a9f46678fb614dcc4ea52871a
NdichuKevin <Kariukikevin98@gmail.com>
2022-07-11 12:36:38 -0700

    Pointers and arrays programs

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,10 @@
 #include <studio.h>
 /**
  *print_rev - Prints a string in reverse
- *@s:The string to print
+ *@s:The string to print 
  *Return:void
 */
+Return:void
 void print_rev(char *s)
 {
 	int c = 0;
@@ -12,7 +13,7 @@ void print_rev(char *s)
 	while (s[c] != '\0')
 	{
 	c++;
-	}	
+	}
 	for (c -= 1; c >= 0; c--)
 	{
 	_putchar(s[c]);
