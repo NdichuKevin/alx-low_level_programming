4352b717898e5b71e98581a033e21dcb350587b0
NdichuKevin <Kariukikevin98@gmail.com>
2022-07-11 01:40:51 -0700

    Nested loops correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 include <stdio.h>
 /**
- *print_number - Prints an integer
- *@n:The integer to prints.
- *Return:Nothing!
+ *print_number - Prints an integer.
+ *@n: The integer to prints.
+ *Return: Nothing!
 */
 void print_number(int n)
 {
