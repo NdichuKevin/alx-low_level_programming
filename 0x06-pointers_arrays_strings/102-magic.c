a5f176f6e4b426c84d1fe73d25dab86fe342405c
NdichuKevin <Kariukikevin98@gmail.com>
2022-07-15 02:57:05 -0700

    Magic indeed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,26 +1,21 @@
-#include "main.h"
-/**
- *infinite_add - Adds two numbers.
- *@n1:The first number to be added.
- *@n2:The second number to be added. @r: The buffer to store the result.
- *@size_r:The buffer size.
- *Return:If r can store the sum - a pointer to the result.
- *If r cannot store the sum - 0.
-*/
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int main(void)
 {
-	int index, n1_len = 0, n2_len = 0;
+  int n;
+  int a[5];
+  int *p;
 
-	for (index = 0; *(n1 + index); index++)
-	n1_len++;
-	for (index = 0; *(n2 + index); index++)
-	n2_len++;
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-	return (0);
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0';
-
-	return (add_strings(n1, n2, r, --size_r));
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
