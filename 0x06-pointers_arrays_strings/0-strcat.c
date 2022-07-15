9c1022a17e92fb57b08d48532eab53b8072bac75
NdichuKevin <Kariukikevin98@gmail.com>
2022-07-14 23:36:25 -0700

    My lines of code that streess

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,9 @@
 #include "main.h"
 #include "stdio.h"
-
 /**
  *_strcat-Concatenates two strings
  *@dest:Thedestination string
  *@src:THe source string
- *
  *Return:A pointer to the resulting string dest
 */
 
@@ -17,7 +15,7 @@ char *_strcat(char *dest, char *src)
 	{
 	i++;
 	}
-	for (j = 0; src[j] !=0; j++)
+	for (j = 0; src[j] != 0; j++)
 	{
 	dest[i] = src[j];
 	i++;
