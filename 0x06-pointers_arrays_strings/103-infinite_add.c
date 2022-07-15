8abdbbb38dbcfa0ccee8f007b21260927d2110b2
NdichuKevin <Kariukikevin98@gmail.com>
2022-07-15 01:05:34 -0700

    My magic codes

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,7 +5,7 @@
  *@n2:The second number to be added. @r: The buffer to store the result.
  *@size_r:The buffer size.
  *Return:If r can store the sum - a pointer to the result.
- *If r cannot store the sum - 0.
+ *	 If r cannot store the sum - 0.
 */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
